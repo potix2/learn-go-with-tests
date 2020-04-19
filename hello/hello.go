e130db52b2b3bd78f9e82186d1f1b435c2b927c8
Katsunori Kanda <potix2@gmail.com>
2020-04-19 16:21:38 +0900

    Add language selector
    Currently the following languages are available:
    
    * English
    * Spanish
    * French

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHello = "Hello"
+const spanishHello = "Hola"
+const frenchHello = "Bonjour"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if len(name) == 0 {
 		name = "World"
 	}
-	return fmt.Sprintf("%s, %s", englishHello, name)
+	prefix := englishHello
+	switch language {
+	case spanish:
+		prefix = spanishHello
+	case french:
+		prefix = frenchHello
+	}
+	return fmt.Sprintf("%s, %s", prefix, name)
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,6 +7,7 @@ func TestHello(t *testing.T) {
 		name string
 		want string
 		arg  string
+		arg2 string
 	}{
 		{
 			name: "SpecificName",
@@ -18,10 +19,22 @@ func TestHello(t *testing.T) {
 			want: "Hello, World",
 			arg:  "",
 		},
+		{
+			name: "InSpanish",
+			want: "Hola, Elodie",
+			arg:  "Elodie",
+			arg2: "Spanish",
+		},
+		{
+			name: "InFrench",
+			want: "Bonjour, Luis",
+			arg:  "Luis",
+			arg2: "French",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Hello(tt.arg); got != tt.want {
+			if got := Hello(tt.arg, tt.arg2); got != tt.want {
 				t.Errorf("Hello() = %v, want %v", got, tt.want)
 			}
 		})
